7a8e81850e0f00fb72c6b3686d0e696df724682d
Nathan Delcambre <ndelcambre@iutbayonne.univ-pau.fr>
2022-01-05 19:31:45 +0100

    Finalisation du fichier de données de test et implémentation dans la BDD

--- src/DataFixtures/Donnees.php
+++ src/DataFixtures/Donnees.php
@@ -8,32 +8,35 @@ use App\Entity\Entreprise;
 use App\Entity\Formation;
 use App\Entity\Stage;
 
-class DonneesEntreprise extends Fixture
+class Donnees extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
         $faker = \Faker\Factory::create('fr_FR');
 
         $lesNomsEntreprises = array("Microsoft", "Apple", "Orange", "SuperDry","Subway", "CocaCola", "SFR", "AirBus","BlackBerry","NavalGroup");
-        $lesFormations = array("DUT Informatique", "DUT Génie Civil", "LP Programmation", "Master Aéronautique","Master Biologie", "Master Chimie", "DUT Tech de Co", "DUT Génie Électrique","Centrale Nantes","Mines Nantes");
+        $lesNomsFormations = array("DUT Informatique", "DUT Génie Civil", "LP Programmation", "Master Aéronautique","Master Biologie", "Master Chimie", "DUT Tech de Co", "DUT Génie Électrique","Centrale Nantes","Mines Nantes");
+        $lesDiminutifsFormations = array("DUT Info", "DUT GC", "LP Prog", "Mast Aéro","Mast Bio", "Mast Chi", "DUT TC", "DUT GE","Cent Nan","Min Nan");
         $lesEntreprises=array();
+        $lesFormations=array();
 
-        for($i=0; $i < count($lesEntreprises); $i=$i+1)
+        for($i=0; $i < count($lesNomsEntreprises); $i=$i+1)
         {
             $entreprise = new Entreprise();
             $entreprise->setActivite($faker->safeEmail());
             $entreprise->setAdresse($faker->streetAddress());
             $entreprise->setNom($lesNomsEntreprises[$i]);
             $entreprise->setUrlSite($faker->domainName());
-            $lesEntreprises[$i]=$entreprise;
+            $lesEntreprises[$i]=$entreprise;    //Ajout des entreprises dans le tableau
             $manager->persist($entreprise);
         }
 
-        for($i=0; $i < count($lesFormations); $i=$i+1)
+        for($i=0; $i < count($lesNomsFormations); $i=$i+1)
         {
             $formation = new Formation();
-            $formation->setNomLong($lesFormations[$i]);
-            $formation->setNomCourt($faker->regexify('[A-Za-z]{4}'));
+            $formation->setNomLong($lesNomsFormations[$i]);
+            $formation->setNomCourt($lesDiminutifsFormations[$i]);
+            $lesFormations[$i]=$formation;    //Ajout des formations dans le tableau
             $manager->persist($formation);
         }
 
@@ -42,12 +45,11 @@ class DonneesEntreprise extends Fixture
         for($i=1; $i <= $nbStages; $i=$i+1)
         {
             $stage = new Stage();
-            $stage->setTitre($faker->sentence());
-            $stage->setDescription($faker->text(200));
+            $stage->setTitre($faker->realText($maxNbChars = 60, $indexSize = 2));
+            $stage->setDescription($faker->realText($maxNbChars = 250, $indexSize = 2));
             $stage->setEmail($faker->safeEmail());
-            $stage->setEntreprise($faker->randomDigit());
-            $stage->setFormation($faker->randomDigit());
-
+            $stage->setEntreprise($lesEntreprises[rand(0,9)]);
+            $stage->addFormation($lesFormations[rand(0,9)]);
             $manager->persist($stage);
         }
         $manager->flush();
