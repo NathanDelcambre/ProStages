95b6de4ce78d5c493e5e80efd5cb96cc164fd954
Nathan Delcambre <ndelcambre@iutbayonne.univ-pau.fr>
2022-01-05 15:51:26 +0100

    Installation de faker

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -125,6 +125,10 @@ class ComposerStaticInit094e5e7459ed6320ff43eb17949da757
         array (
             'Laminas\\Code\\' => 13,
         ),
+        'F' => 
+        array (
+            'Faker\\' => 6,
+        ),
         'E' => 
         array (
             'Egulias\\EmailValidator\\' => 23,
@@ -483,6 +487,10 @@ class ComposerStaticInit094e5e7459ed6320ff43eb17949da757
         'Laminas\\Code\\' => 
         array (
             0 => __DIR__ . '/..' . '/laminas/laminas-code/src',
+        ),
+        'Faker\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/fzaninotto/faker/src/Faker',
         ),
         'Egulias\\EmailValidator\\' => 
         array (
